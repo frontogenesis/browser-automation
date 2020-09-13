0786023a972ed16e1846f7eb64847ff43864af7b
Ray Hawthorne <frontogenesis@gmail.com>
2020-09-13 15:20:25 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,10 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  
+  await driver.get('http://www.google.com/ncr');
+  await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+  await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+
+})();
